faad423d2825ac7f4777016033da0d298884f38e
durga207 <87663826+durga207@users.noreply.github.com>
2021-12-16 09:36:20 +0530

    Cop number added

--- src/Main.java
+++ src/Main.java
@@ -12,30 +12,30 @@ public class Main {
 
         System.out.print("Enter number of vertices: ");
         N = sc.nextInt();
-        
+        Graphs graph1 = new Graphs(N);
+
         System.out.print("Enter number of edges: ");
         E = sc.nextInt();
         System.out.println();
 
         int[] U = new int[E];
         int[] V = new int[E];
-        ArrayList<ArrayList<Integer>>adj = new ArrayList<ArrayList<Integer> >();
-        for(int i = 0;i<N;i++)
-        {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
+        for (int i = 0; i < N; i++) {
             adj.add(new ArrayList<Integer>());
         }
-        for(int i = 0; i < E; i++){
+        for (int i = 0; i < E; i++) {
             do {
                 System.out.print("First vertex number: ");
                 U[i] = sc.nextInt();
-            }while (!(U[i] > 0 && U[i] <= N));
+            } while (!(U[i] > 0 && U[i] <= N));
 
             do {
                 System.out.print("Connected vertex number: ");
                 V[i] = sc.nextInt();
-            }while (!(V[i] > 0 && V[i] <= N));
-            adj.get(U[i]-1).add(V[i]-1);
-            adj.get(V[i]-1).add(U[i]-1);
+            } while (!(V[i] > 0 && V[i] <= N));
+            adj.get(U[i] - 1).add(V[i] - 1);
+            adj.get(V[i] - 1).add(U[i] - 1);
             System.out.println();
         }
         // For testing remove later....
@@ -47,28 +47,32 @@ public class Main {
         //     }
         //     System.out.println();
         // }
-        if(CompleteGraph.checkCompleteGraph(N, E, U, V)){
+        if (CompleteGraph.checkCompleteGraph(N, E, U, V)) {
             System.out.println("COP-WIN graph\nIt is a complete graph\nIn a complete graph, every vertex is connected to every other vertex of the graph\n");
         }
 
         /**if true, it is definitely ROBBER WIN graph
          * if false, we cannot conclude anything*/
         else if (CyclicGraph.checkCyclicGraph(N, E, U, V)) {
-            System.out.println("The graph is robber win as it is cyclic\nCop number = 2");
-        }
-
-        else if(TreeGraph.isTree(N, U, V)) {
+            System.out.println("The graph is robber win as it is cyclic");
+            for (int i = 0; i < U.length; i++) {
+                graph1.addingEdge(U[i] - 1, V[i] - 1);
+            }
+            graph1.greedyColorNodes();
+        } else if (TreeGraph.isTree(N, U, V)) {
             System.out.println("Cop-win Graph.\nCop number = 1");
-        }
-        else{
+        } else {
             Cop_n_robber test = new Cop_n_robber();
-            test.checkCycle(N,adj);
-            if(test.rob == true)
-            System.out.println("Robber winning graph");
-            else
-            System.out.println("Cop winning graph");
+            test.checkCycle(N, adj);
+            if (test.rob == true) {
+                System.out.println("Robber winning graph");
+                for (int i = 0; i < U.length; i++) {
+                    graph1.addingEdge(U[i] - 1, V[i] - 1);
+                }
+                graph1.greedyColorNodes();
+            } else
+                System.out.println("Cop winning graph");
         }
 
-        copNumber.minimumCops(N, E, U, V);
     }
 }
